fa82f18ce63ad90cb2c773a1091f77cbd3f4dcbb
linxiaozhi <linxiaozhi10@163.com>
2012-08-07 22:30:27 +0800

    add online_music_fragment

--- Ting/src/com/example/ting/OnlineMusicFragment.java
+++ Ting/src/com/example/ting/OnlineMusicFragment.java
@@ -1,13 +1,24 @@
 package com.example.ting;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.viewpagerindicator.CirclePageIndicator;
 import com.viewpagerindicator.PageIndicator;
@@ -28,7 +39,25 @@ public class OnlineMusicFragment extends Fragment {
 			Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_online, container, false);
 
-		mAdapter = new MyPagerAdapter();
+		String[] files = null;
+		String dir = "album";
+		try {
+			String[] temp = getActivity().getAssets().list(dir);
+			files = new String[temp.length];
+			for (int j = 0; j < temp.length; j++) {
+				files[j] = dir + "/" + temp[j];
+
+			}
+
+			InputStream is = getActivity().getAssets().open(files[0]);
+
+			ImageView image = new ImageView(getActivity());
+			Bitmap bitmap = BitmapFactory.decodeStream(is);
+			image.setImageBitmap(bitmap);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		mAdapter = new MyPagerAdapter(inflater, files);
 		mPager = (ViewPager) v.findViewById(R.id.pager);
 		mPager.setAdapter(mAdapter);
 
@@ -36,30 +65,116 @@ public class OnlineMusicFragment extends Fragment {
 				.findViewById(R.id.indicator);
 		mIndicator = indicator;
 		indicator.setViewPager(mPager);
+		indicator.setFillColor(getResources().getColor(R.color.bg_color));
 		indicator.setSnap(true);
+
+		
+		List<Map<String, String>> list = new ArrayList<Map<String, String>>();
+		String[] s1 = getResources().getStringArray(
+				R.array.online_music_hot_title);
+		String[] s2 = getResources().getStringArray(
+				R.array.online_music_hot_sub_title);
+		int len = s1.length;
+		for (int i = 0; i < len; i++) {
+			Map<String, String> map = new HashMap<String, String>();
+			map.put("title", s1[i]);
+			map.put("subtitle", s2[i]);
+			list.add(map);
+		}
+
+		SimpleAdapter adapter = new SimpleAdapter(getActivity(), list,
+				R.layout.online_music_textview, new String[] { "title",
+						"subtitle" }, new int[] { R.id.textView1,
+						R.id.textView2 });
+		ListView lists = (ListView) v.findViewById(R.id.listview);
+		lists.setAdapter(adapter);
 		return v;
 	}
 
 	class MyPagerAdapter extends PagerAdapter {
+		LayoutInflater inflater;
+		String[] bitmapPath;
+
+		public MyPagerAdapter(LayoutInflater inflater, String[] bitmapPath) {
+			this.inflater = inflater;
+			this.bitmapPath = bitmapPath;
+		}
 
 		@Override
 		public int getCount() {
-			return 0;
+			int len = (int) Math.ceil(bitmapPath.length * 1.0 / 3);
+			return len;
 		}
 
 		@Override
-		public boolean isViewFromObject(View arg0, Object arg1) {
-			return false;
+		public boolean isViewFromObject(View view, Object object) {
+			return view == object;
 		}
+
+		// @Override
+		// public Object instantiateItem(View container, int position) {
+		// View v = inflater.inflate(R.layout.album_image_view,
+		// null);
+		//
+		// ImageView view1 = (ImageView) v.findViewById(R.id.imageView1);
+		// ImageView view2 = (ImageView) v.findViewById(R.id.imageView1);
+		// ImageView view3 = (ImageView) v.findViewById(R.id.imageView1);
+		// InputStream is;
+		// try {
+		// is = getActivity().getAssets().open(bitmapPath[position]);
+		// Bitmap bitmap = BitmapFactory.decodeStream(is);
+		// view1.setImageBitmap(bitmap);
+		// view2.setImageBitmap(bitmap);
+		// view3.setImageBitmap(bitmap);
+		// } catch (IOException e) {
+		// e.printStackTrace();
+		// }
+		//
+		// return v;
+		// }
 
 		@Override
 		public Object instantiateItem(ViewGroup container, int position) {
-			return super.instantiateItem(container, position);
+			View v = inflater.inflate(R.layout.album_image_view, container,
+					false);
+
+			ImageView view1 = (ImageView) v.findViewById(R.id.imageView1);
+			ImageView view2 = (ImageView) v.findViewById(R.id.imageView2);
+			ImageView view3 = (ImageView) v.findViewById(R.id.imageView3);
+			InputStream is = null;
+			Bitmap bitmap;
+			try {
+				int index = position * 3;
+				int len = bitmapPath.length;
+				if (index < len) {
+					is = getActivity().getAssets().open(bitmapPath[index]);
+					bitmap = BitmapFactory.decodeStream(is);
+					view1.setImageBitmap(bitmap);
+				}
+
+				index++;
+				if (index < len) {
+					is = getActivity().getAssets().open(bitmapPath[index]);
+					bitmap = BitmapFactory.decodeStream(is);
+					view2.setImageBitmap(bitmap);
+				}
+
+				index++;
+				if (index < len) {
+					is = getActivity().getAssets().open(bitmapPath[index]);
+					bitmap = BitmapFactory.decodeStream(is);
+					view3.setImageBitmap(bitmap);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			((ViewPager) container).addView(v);
+			return v;
 		}
 
 		@Override
 		public void destroyItem(ViewGroup container, int position, Object object) {
-			super.destroyItem(container, position, object);
+			container.removeView((View) object);
 		}
 	}
 
