f668d4cd239f39a25c7e162d4295327e63c9f8ae
linxiaozhi <linxiaozhi10@163.com>
2012-08-07 09:28:55 +0800

    v01.add all files

---
+++ Ting/src/com/example/ting/SecondFragment.java
@@ -0,0 +1,24 @@
+package com.example.ting;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SecondFragment extends Fragment {
+
+  
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+      
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+    	 View v = inflater.inflate(R.layout.second_layout, container, false);
+        return v;
+    }
+}
